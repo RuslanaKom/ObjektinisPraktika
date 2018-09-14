20b9b6490852074f4afc92f6d6a6364689c972ab
Ruslana Komaristova <rkomaristova@exigengroup.com>
2018-09-14 10:57:10 +0300

    Second task added

--- Objektinis_Praktika/src/praktika/main/Main.java
+++ Objektinis_Praktika/src/praktika/main/Main.java
@@ -2,11 +2,15 @@ package praktika.main;
 
 import java.util.Map;
 
+import org.junit.jupiter.api.Test;
+
 import praktika.tasks.first.Rectangle;
+import praktika.tasks.second.Chronometer;
 
 public class Main {
-    public static void main(String[] args) {
 
+    @Test
+    public void testRectangle() {
         Rectangle rectangle = new Rectangle();
         rectangle.setLength(12D);
         rectangle.setWidth(4D);
@@ -14,7 +18,7 @@ public class Main {
 
         System.out.println("Area:" + rectangle.getArea());
         System.out.println("Perimeter:" + rectangle.getPerimeter());
-        System.out.println("Diagonal:" + rectangle.getDiagonal());
+        System.out.printf("Diagonal: %.2f\n",rectangle.getDiagonal());
         System.out.println("Intersection coordinates: " + rectangle.getDiagonalIntersection().get("x") + ","
                 + rectangle.getDiagonalIntersection().get("y"));
         Map<String, Double> corners = rectangle.getAllCorners();
@@ -23,4 +27,60 @@ public class Main {
         System.out.println("Bottom left corner: " + corners.get("xBL") + "," + corners.get("yBL"));
         System.out.println("Uppe left corner: " + corners.get("xBR") + "," + corners.get("yBR"));
     }
+
+    @Test
+    public void testChronometerHappyPath() throws InterruptedException {
+        Chronometer chronometer = new Chronometer();
+        chronometer.start();
+        Thread.sleep(3000);
+        System.out.println(chronometer.getIntermediate());
+        Thread.sleep(2000);
+        System.out.println(chronometer.getIntermediate());
+        Thread.sleep(3000);
+        chronometer.pause();
+        Thread.sleep(1000);
+        chronometer.pause();
+        System.out.println(chronometer.getIntermediate());
+        System.out.println(chronometer.stop());
+    }
+
+    @Test
+    public void testChronometerRandomPath() throws InterruptedException {
+        Chronometer chronometer = new Chronometer();
+        chronometer.start();
+        Thread.sleep(3000);
+        chronometer.pause();
+        Thread.sleep(2000);
+        System.out.println(chronometer.getIntermediate());
+        Thread.sleep(2000);
+        System.out.println(chronometer.getIntermediate());
+        Thread.sleep(3000);
+        chronometer.pause();
+        Thread.sleep(1000);
+        System.out.println(chronometer.getIntermediate());
+        chronometer.pause();
+        Thread.sleep(1000);
+        System.out.println(chronometer.stop());
+    }
+
+    @Test
+    public void testSeveralPausesBetweenIntermediates() throws InterruptedException {
+        Chronometer chronometer = new Chronometer();
+        chronometer.start();
+        Thread.sleep(3000);
+        System.out.println(chronometer.getIntermediate());
+        Thread.sleep(2000);
+        System.out.println(chronometer.getIntermediate());
+        Thread.sleep(1);
+        chronometer.pause();
+        Thread.sleep(1000);
+        chronometer.pause();
+        Thread.sleep(3000);
+        chronometer.pause();
+        Thread.sleep(2000);
+        chronometer.pause();
+        System.out.println(chronometer.getIntermediate());
+        System.out.println(chronometer.stop());
+    }
+
 }
---
+++ Objektinis_Praktika/src/praktika/tasks/second/Chronometer.java
@@ -0,0 +1,104 @@
+package praktika.tasks.second;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+
+public class Chronometer {
+
+    private LocalTime startTime;
+
+    private LocalTime stopTime;
+
+    private LocalTime intermediateTime;
+
+    private LocalTime pauseStart;
+
+    private LocalTime pauseEnd;
+
+    private Duration pause = Duration.ZERO;
+
+    private Duration pausesBetweenIntermediates = Duration.ZERO;
+
+    private static int pauseCounter = 0;
+
+    private boolean chronometerStarted = false;
+
+    SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss");
+
+    public void start() {
+        startTime = LocalTime.now();
+        pause = Duration.ZERO;
+        intermediateTime = startTime;
+        pauseStart = LocalTime.now();
+        pauseEnd = LocalTime.now();
+        chronometerStarted = true;
+    }
+
+    public String getIntermediate() {
+        Duration activePause = Duration.ZERO;
+        if (chronometerStarted) {
+            LocalTime intermediateTimeTemp = LocalTime.now();
+            if (pauseCounter == 1 && intermediateTime.isBefore(pauseStart)) {
+                activePause = Duration.between(pauseStart, intermediateTimeTemp);
+            }
+            else if (pauseCounter == 1 && intermediateTime.isAfter(pauseStart)) {
+                activePause = Duration.between(intermediateTime, intermediateTimeTemp);
+            }
+            else if (pauseCounter == 0 && intermediateTime.isAfter(pauseStart) && intermediateTime.isBefore(pauseEnd)) {
+                activePause = Duration.between(intermediateTime, pauseEnd);
+            }
+            Duration dur = Duration.between(intermediateTime, intermediateTimeTemp).minus(pausesBetweenIntermediates)
+                    .minus(activePause);
+            intermediateTime = intermediateTimeTemp;
+            pausesBetweenIntermediates = Duration.ZERO;
+
+            return formatTime(dur.getSeconds());
+        }
+        else {
+            return "0";
+        }
+    }
+
+    public void pause() {
+        if (chronometerStarted) {
+            if (pauseCounter == 0) {
+                pauseStart = LocalTime.now();
+                pauseCounter = 1;
+            }
+            else {
+                pauseEnd = LocalTime.now();
+                pause = pause.plus(Duration.between(pauseStart, pauseEnd));
+                if (pauseStart.isAfter(intermediateTime)) {
+                    pausesBetweenIntermediates = pausesBetweenIntermediates.plus(Duration.between(pauseStart, pauseEnd));
+                }
+                pauseCounter = 0;
+            }
+        }
+    }
+
+    public String stop() {
+        if (chronometerStarted) {
+            stopTime = LocalTime.now();
+            if (pauseCounter == 1) {
+                pause = pause.plus(Duration.between(pauseStart, stopTime));
+            }
+            Duration dur = Duration.between(startTime, stopTime).minus(pause);
+            chronometerStarted = false;
+            return formatTime(dur.getSeconds());
+        }
+        else {
+            return "0";
+        }
+    }
+
+    private String formatTime(long seconds) {
+        long absSeconds = Math.abs(seconds);
+        String intermTime = String.format("%d:%02d:%02d", absSeconds / 3600, (absSeconds % 3600) / 60, absSeconds % 60);
+        return intermTime;
+    }
+}
