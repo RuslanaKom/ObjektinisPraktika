d2bec5da4677cf8ab80d6c631c79e8f76d27c4d3
Ruslana Komaristova <rkomaristova@exigengroup.com>
2019-02-18 10:16:52 +0200

    vineger line scan added

--- InfoSecurity/src/vinegere/Main.java
+++ InfoSecurity/src/vinegere/Main.java
@@ -1,9 +1,28 @@
 package vinegere;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Cipher cipher = new Cipher();
-        System.out.println(cipher.decode(cipher.encode("This is a testing text", "aete"), "aete"));
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Text to encode: ");
+        String  text= scan.nextLine();
+        System.out.println("Key: ");
+        String  key= scan.nextLine();
+        
+        System.out.println(cipher.encode(text, key));
+        
+        System.out.println("Text to decode: ");
+        String  text2= scan.nextLine();
+        System.out.println("Key: ");
+        String  key2= scan.nextLine();
+        
+        System.out.println(cipher.decode(text2, key2)); 
+
+        
+       // System.out.println(cipher.decode(cipher.encode("This is a testing text", "po"), "po"));
     }
 }
